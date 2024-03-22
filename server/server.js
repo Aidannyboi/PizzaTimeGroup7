1081ba6892e1cfe6616f2817789e4fb437bba9cf
Leonardo <leonardo.mejiam@hotmail.com>
2024-03-22 10:23:50 -0700

    Updated server

--- server/server.js
+++ server/server.js
@@ -2,14 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import dbConnect from './config/mongoose.config.js';
-//import router from './routes/user.routes.js';
-//import router_photos from './routes/photo.routes.js';
-//import router_album from './routes/album.routes.js'
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser'
+
+
 
 const app = express();
-app.use(cookieParser());
 app.use(express.json(), cors({credentials: true,origin: 'http://localhost:5173' }));
 
 
